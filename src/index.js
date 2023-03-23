98e9805e52b6dd528ad0202f636254c966436957
Dima Maiboroda <32996912+Dimaybor@users.noreply.github.com>
2023-03-23 18:37:22 +0200

    section connection

--- src/index.js
+++ src/index.js
@@ -109,3 +109,21 @@ const swiper4 = new Swiper('.certificates-slider', {
     }
   },
 });
+
+var modal = document.getElementById('myModal');
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("connection-modal__close")[0]; 
+btn.onclick = function() {
+    modal.style.display = "block";
+    document.body.classList.add('modal-open');
+}
+span.onclick = function() {
+    modal.style.display = "none";
+    document.body.classList.remove('modal-open');
+}
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+        document.body.classList.remove('modal-open');
+    }
+}
